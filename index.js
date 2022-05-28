8020ab42954bdb0d72720688ea442388bda96f97
Victoria Parry <vlparry@gmail.com>
2022-05-28 11:26:28 -0400

    commit tree ce7954710b83881c0a6c75dbe1e5c905ff3b197d

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+function reverse(word) {
+  const wordArray = word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord
+}
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+
+  if (word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+* if the word is the same as the word in reverse order, it should return true.
+if not, it should return false.
+needto reverse the input string
+if the input is the same as the reversed input 
+return true
+else
+return false 
 
 /*
   Add written explanation of your solution here
@@ -20,6 +42,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("boob"));
 }
 
 module.exports = isPalindrome;
